39ab33cdf98bdfeb5043d7f8fab1b74afb6783c3
caroquintana <caro.quintana.n@gmail.com>
2017-07-09 18:27:56 -0400

    suena bateria

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 //detectar un evento en el teclado en la ventana del navegador
 window.addEventListener('keydown', function(e) {
-	console.log(e);
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //detecta las teclas del audio data-key del html
+	if(!audio) return; //detiene la función de salir corriendo
+
+	audio.play(); //pone play a audio
 });
