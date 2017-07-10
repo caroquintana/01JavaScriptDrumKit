0058d3da4b7bb2411d01c0dbe77b8876839c17e7
caroquintana <caro.quintana.n@gmail.com>
2017-07-09 20:57:41 -0400

    fin ejercicio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
-//detectar un evento en el teclado en la ventana del navegador
-window.addEventListener('keydown', function(e) {
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //detecta las teclas del audio data-key del html
+ function playSound(e){
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //detecta la etiqueta audio data-key del html
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); //detecta la etiqueta div data-key del html
 	if(!audio) return; //detiene la función de salir corriendo
-
+	audio.currentTime = 0; //permite que se pueda repetir el sonido al presionar varias veces las teclas
 	audio.play(); //pone play a audio
-});
+	//En jquery sería key.addClass('playing');
+	key.classList.add('playing'); //añade al div que tiene clase key, la clase playing (css)
+}
+
+function removeTransition(e){
+	if(e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
+
+	const keys = document.querySelectorAll('.key');
+	keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+//detectar un evento del teclado en la ventana del navegador
+window.addEventListener('keydown', playSound);
